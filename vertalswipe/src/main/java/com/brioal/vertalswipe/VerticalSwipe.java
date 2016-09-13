ace38a7f604ae222d0dee198296361c269cd4e44
Brioal <brioal@foxmail.com>
2016-09-13 08:54:43 +0800

    Initial commit

---
+++ vertalswipe/src/main/java/com/brioal/vertalswipe/VerticalSwipe.java
@@ -0,0 +1,126 @@
+package com.brioal.vertalswipe;
+
+import android.content.Context;
+import android.support.v4.view.ViewCompat;
+import android.support.v4.widget.ViewDragHelper;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.FrameLayout;
+
+/**
+ * Vertal Swipe Content
+ * Created by Brioal on 2016/9/13.
+ */
+public class VerticalSwipe extends FrameLayout {
+    private View mMainView;
+    private View mSwipeView;
+    private ViewDragHelper mDragHelper;
+    int mMainHeight;
+    int mSwipeHeight;
+
+
+    public VerticalSwipe(Context context) {
+        this(context, null);
+    }
+
+    public VerticalSwipe(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    private void init() {
+        mDragHelper = ViewDragHelper.create(this, new ViewDragHelper.Callback() {
+            @Override
+            public boolean tryCaptureView(View child, int pointerId) {
+                if (child == mSwipeView) { //SwipeView允许滑动
+                    return true;
+                }
+                return false;
+            }
+
+            @Override
+            public int clampViewPositionVertical(View child, int top, int dy) {
+                if (top > mMainHeight) {
+                    return mMainHeight;
+                }
+                if (top < getMeasuredHeight() - mSwipeView.getMeasuredHeight()) {
+                    if (top < 0) {
+                        return 0;
+                    }
+                    return getMeasuredHeight() - mSwipeView.getMeasuredHeight();
+                }
+                return top;
+            }
+
+            @Override
+            public void onViewReleased(View releasedChild, float xvel, float yvel) {
+                super.onViewReleased(releasedChild, xvel, yvel);
+                int topffset = getMeasuredHeight() - mSwipeView.getMeasuredHeight();
+                if (topffset < 0) {
+                    topffset = 0;
+                }
+                if (mSwipeView.getTop() > mMainHeight * 3 / 5) {
+                    mDragHelper.smoothSlideViewTo(mSwipeView, 0, mMainHeight); //滑动到原本位置
+                    ViewCompat.postInvalidateOnAnimation(VerticalSwipe.this);
+                } else {
+                    mDragHelper.smoothSlideViewTo(mSwipeView, 0, topffset); //滑动到原本位置
+                    ViewCompat.postInvalidateOnAnimation(VerticalSwipe.this);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void computeScroll() {
+        super.computeScroll();
+        if (mDragHelper.continueSettling(true)) {
+            ViewCompat.postInvalidateOnAnimation(this);
+        }
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        mMainView = getChildAt(0);
+        mSwipeView = getChildAt(1);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        return mDragHelper.shouldInterceptTouchEvent(ev);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        mDragHelper.processTouchEvent(event);
+        return true;
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        measureChild(mMainView, widthMeasureSpec, heightMeasureSpec);
+        measureChild(mSwipeView, widthMeasureSpec, heightMeasureSpec);
+        int width = MeasureSpec.getSize(widthMeasureSpec);
+        int height = MeasureSpec.getSize(heightMeasureSpec);
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        setMeasuredDimension(width, height);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mMainHeight = mMainView.getMeasuredHeight();
+        mSwipeHeight = mSwipeView.getMeasuredHeight();
+        System.out.println("MainHeight:" + mMainHeight);
+        System.out.println("SwipeHeight:" + mSwipeView.getMeasuredHeight());
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        System.out.println("MainHeight:" + mMainHeight);
+        System.out.println("SwipeHeight:" + mSwipeHeight);
+        mMainView.layout(0, 0, getMeasuredWidth(), mMainHeight);
+        mSwipeView.layout(0, mMainHeight, getMeasuredWidth(), mMainHeight + mSwipeHeight);
+    }
+}
