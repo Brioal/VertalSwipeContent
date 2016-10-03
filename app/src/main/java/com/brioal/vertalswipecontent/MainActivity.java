3ab11413a9d99e33ba077e1da34b1209c566cb18
Brioal <brioal@foxmail.com>
2016-10-03 10:15:15 +0800

    修改后的效果演示

--- app/src/main/java/com/brioal/vertalswipecontent/MainActivity.java
+++ app/src/main/java/com/brioal/vertalswipecontent/MainActivity.java
@@ -4,20 +4,28 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.brioal.vertalswipe.VerticalSwipe;
+import com.brioal.vertalswipe.interfaces.OnSwipeListener;
 
 public class MainActivity extends AppCompatActivity {
     VerticalSwipe mContainer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mContainer = (VerticalSwipe) findViewById(R.id.container);
+        //设置滑动界面滑动监听器0~1.0f
+        mContainer.setSwipeListener(new OnSwipeListener() {
+            @Override
+            public void progress(float progress) {
+
+            }
+        });
 
     }
 
     @Override
     public void onBackPressed() {
-
         if (mContainer.isSwipeViewShowing()) {
             mContainer.setSwipeViewShowing(false);
         } else {
